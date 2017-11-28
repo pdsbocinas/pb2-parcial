762052acd1ef9b9a0714611c103badccc6dbf042
pds.bocinas <pablo@handerr.com>
2017-11-28 15:16:02 -0300

    parcial

---
+++ src/contendedor/unlam/com/ar/Contenedor.java
@@ -0,0 +1,70 @@
+package contendedor.unlam.com.ar;
+
+import java.util.LinkedList;
+
+public class Contenedor {
+	private Integer cantidadMaxima;
+	private Integer maximo = 0;
+	private LinkedList<Elemento> elementos;
+	
+	public Contenedor(Integer cantidadMaxima) {
+		this.cantidadMaxima = cantidadMaxima;
+		elementos = new LinkedList<Elemento>();
+	}
+	
+	public Integer getCantidadMaxima() {
+		return cantidadMaxima;
+	}
+	
+	public void setCantidadMaxima(Integer cantidadMaxima) {
+		this.cantidadMaxima = cantidadMaxima;
+	}
+	
+	public Integer getMaximo() {
+		return maximo;
+	}
+	
+	public void setMaximo(Integer maximo) {
+		this.maximo = maximo;
+	}
+	
+	public LinkedList<Elemento> getElementos() {
+		return elementos;
+	}
+	
+	public void setElementos(LinkedList<Elemento> elementos) {
+		this.elementos = elementos;
+	}
+	
+	public boolean agregarElemento (Elemento elemento) {
+        if(elementos.size() >= cantidadMaxima) {
+            return false;
+        } else {
+        		elementos.add(elemento);
+            
+        		if(elemento.getPeso() > maximo) {
+                	this.maximo = elemento.getPeso();
+            }
+        	    return true;
+        }   
+    }
+	
+	public Integer contarElementos () {
+		return elementos.size();
+	}
+	
+	public Integer calcularPeso () {
+		int pesoTotal = 0;
+		
+		for(Elemento item : elementos) {
+            pesoTotal = pesoTotal + item.getPeso();
+        }
+		
+		return pesoTotal;
+	}
+	
+	public Integer mayorPeso() {
+		return this.maximo;
+	}
+	
+}
---
+++ src/contendedor/unlam/com/ar/Elemento.java
@@ -0,0 +1,23 @@
+package contendedor.unlam.com.ar;
+
+public class Elemento {
+	private Integer id;
+	private Integer peso;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getPeso() {
+		return peso;
+	}
+	
+	public void setPeso(Integer peso) {
+		this.peso = peso;
+	}
+	
+}
